ca6f55511350cafcd72f681d7bc0a6a686b44108
VengantMjolnir <vengantmjolnir@gmail.com>
2016-05-15 20:18:27 -0700

    Initial commit of project files

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 offset;
+    private Vector3 currentVelocity;
+
+    public float smoothTime = 0.3f;
+    public float speedAdjustment = 0.25f;
+    public Transform player;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = this.transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Rigidbody rb = player.GetComponent<Rigidbody>();
+        Vector3 target = player.position + offset + (rb.velocity * speedAdjustment);
+        this.transform.position = Vector3.SmoothDamp(transform.position, target, ref currentVelocity, smoothTime);
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody cachedRigidbody;
+
+	// Use this for initialization
+	void Start () {
+        cachedRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        cachedRigidbody.AddForce(h * speed,0,v * speed);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+}
